03d9f2ae7371bfbfff7fbf38039a7797a0fed976
Rakhmetzhanova DIlnaz <dilnazorum@mail.ru>
2026-06-01 02:57:59 +0300

    add UTC_OFFSET config

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ MANGABUFF_EMAIL: str = os.getenv("MANGABUFF_EMAIL", "")
 MANGABUFF_PASSWORD: str = os.getenv("MANGABUFF_PASSWORD", "")
 DAILY_READ_LIMIT: int = int(os.getenv("DAILY_READ_LIMIT", "50"))
 COMMENT_INTERVAL_MIN: int = int(os.getenv("COMMENT_INTERVAL_MIN", "5"))
+UTC_OFFSET: int = int(os.getenv("UTC_OFFSET", "5"))
 
 MANGABUFF_BASE: str = "https://mangabuff.ru"
 USER_AGENT: str = (
